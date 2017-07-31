c7d000949831dffc8b08a02a95d99e3a848c3fdc
tosmak <tosmak16@gmail.com>
2017-07-31 21:36:27 +0100

    add comment

--- src/app.js
+++ src/app.js
@@ -2,31 +2,36 @@
 
 
 function aritGeo(a) {
+
+    var ap = 1;
+    var gp = 1;
+    //this line is to check if the value input as array is null or void so as to avoid crashing the app
     if (a == null) {
         return "no parameters passed";
-    } else if (!Array.isArray(a)) {
+    }
+    //this line is to check if the input value is an array 
+    else if (!Array.isArray(a)) {
         return 'invalid input';
     } else {
-
+        //this line of code checks if the array is empty
         if (a.length === 0) {
             return 0;
         }
         var commonDiff = a[1] - a[0];
         var commonRatio = a[1] / a[0];
 
-        var ap = true;
-        var gp = true;
-
         for (var i = 0; i < a.length - 1; i++) {
             if (a[i + 1] - a[i] !== commonDiff)
-                ap = false;
+                ap = 0;
             if (a[i + 1] / commonRatio !== a[i])
-                gp = false;
+                gp = 0;
         }
-
-        if (ap === true)
+        //this line of code check if Arithmetic progression is true
+        if (ap === 1)
             return "Arithmetic";
-        else if (gp === true)
+        // this line of code checks if Geometric progression is true
+        else if (gp === 1)
+        //this line of code checks if neither AP or GP is true
             return "Geometric";
         else
             return -1;
