a0d663a85b6e28358e65622737868efba1ba2604
tosmak <tosmak16@gmail.com>
2017-07-31 16:08:56 +0100

    check for invalid typeError and vold parameter commit

--- src/app.js
+++ src/app.js
@@ -2,29 +2,35 @@
 
 
 function aritGeo(a) {
-    if (a.length === 0) {
-        return 0;
+    if (a == null) {
+        return "no parameters passed";
+    } else if (!Array.isArray(a)) {
+        return 'invalid input';
+    } else {
+
+        if (a.length === 0) {
+            return 0;
+        }
+        var commonDiff = a[1] - a[0];
+        var commonRatio = a[1] / a[0];
+
+        var ap = true;
+        var gp = true;
+
+        for (var i = 0; i < a.length - 1; i++) {
+            if (a[i + 1] - a[i] !== commonDiff)
+                ap = false;
+            if (a[i + 1] / commonRatio !== a[i])
+                gp = false;
+        }
+
+        if (ap === true)
+            return "Arithmetic";
+        else if (gp === true)
+            return "Geometric";
+        else
+            return -1;
     }
-
-    var commonDiff = a[1] - a[0];
-    var commonRatio = a[1] / a[0];
-
-    var ap = true;
-    var gp = true;
-
-    for (var i = 0; i < a.length - 1; i++) {
-        if (a[i + 1] - a[i] !== commonDiff)
-            ap = false;
-        if (a[i + 1] / commonRatio !== a[i])
-            gp = false;
-    }
-
-    if (ap === true)
-        return "Arithmetic";
-    else if (gp === true)
-        return "Geometric";
-    else
-        return -1;
 
 }
 module.exports.aritGeo = aritGeo;
--- test/test.js
+++ test/test.js
@@ -12,23 +12,32 @@ describe("Determine the sequence of an array of numbers: ", () => {
         })
         // To test for if AP is valid
     describe("check if the array entered is an arithmetic progression ", () => {
-        it("should return Arithmetic", () => {
-            assert.equal(app.aritGeo([1, 2]), "Arithmetic");
+            it("should return Arithmetic", () => {
+                assert.equal(app.aritGeo([1, 2]), "Arithmetic");
+            })
         })
-    })
-
+        //To test for if GP is valid
     describe("check if the array entered is a geometric progression", () => {
-        it("should return Geometric", () => {
-            assert.equal(app.aritGeo([2, 4, 8]), "Geometric");
+            it("should return Geometric", () => {
+                assert.equal(app.aritGeo([2, 4, 8]), "Geometric");
+            })
+        })
+        //To test for if neither AP or GP
+    describe("check if the array entered is neither geometric nor arithmetic progression", () => {
+            it("should return -1", () => {
+                assert.equal(app.aritGeo([1, 3, 7, 10]), -1);
+            })
+        })
+        //To test for invalid input
+    describe("check if the value entered is not an array", () => {
+            it("should return invalid input ", () => {
+                assert.equal(app.aritGeo(1), "invalid input");
+            })
+        })
+        //To test if the value is null
+    describe("check if the value is null", () => {
+        it("should return invalid input ", () => {
+            assert.equal(app.aritGeo(), "no parameters passed");
         })
     })
-    describe("if the array entered is neither geometric nor arithmetic progression", () => {
-        it("should return -1", () => {
-            assert.equal(app.aritGeo([1, 3, 7, 10]), -1);
-        })
-    })
-
-
-
-
 })
