be6925211493c93468442a3d9b4387848f295b76
tosmak <tosmak16@gmail.com>
2017-07-31 15:04:03 +0100

    Arithmetic progression commit

--- src/app.js
+++ src/app.js
@@ -6,5 +6,20 @@ function aritGeo(a) {
         return 0;
     }
 
+    var commonDiff = a[1] - a[0];
+    var commonRatio = a[1] / a[0];
+
+    var ap = true;
+    var gp = true;
+
+    for (var i = 0; i < a.length - 1; i++) {
+        if (a[i + 1] - a[i] !== commonDiff)
+            ap = false;
+        if (a[i + 1] / commonRatio !== a[i])
+            gp = false;
+    }
+
+    if (ap === true)
+        return "Arithmetic";
 }
 module.exports.aritGeo = aritGeo;
--- test/test.js
+++ test/test.js
@@ -2,14 +2,21 @@ const assert = require("chai").assert;
 const app = require("../src/app.js");
 
 describe("Determine the sequence of an array of numbers: ", () => {
-
+    // To test for empty array
     describe("Check for an empty array", () => {
 
-        it("should return 0 if an empty array is passed", () => {
-            assert.equal(app.aritGeo([]), 0);
-        });
+            it("should return 0 if an empty array is passed", () => {
+                assert.equal(app.aritGeo([]), 0);
+            });
 
+        })
+        // To test for if AP is valid
+    describe(" if the array entered is an arithmetic progression ", () => {
+        it("should return Arithmetic", () => {
+            assert.equal(app.aritGeo([1, 2]), "Arithmetic");
+        })
     })
+
 
 
 
